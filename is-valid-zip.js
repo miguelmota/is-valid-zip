0e91e7407ff5131b2814408e6ba09340dbe99277
Miguel Mota <miguelmota2@gmail.com>
2014-12-01 23:04:14 -0800

    update global

--- is-valid-zip.js
+++ is-valid-zip.js
@@ -1,15 +1,15 @@
 (function(){
 
-  function isValidEmail(v) {
+  function isValidZip(v) {
     if (!v) return false;
     var re = /(^\d{5}$)|(^\d{5}-\d{4}$)/;
     return re.test(v);
   }
 
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = isValidEmail;
+    module.exports = isValidZip;
   } else {
-    window.isValidEmail = isValidEmail;
+    window.isValidZip = isValidZip;
   }
 
 })();
